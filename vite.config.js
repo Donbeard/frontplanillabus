bbefbbf162ddc40fcd21846e4eaaa098dbbea484
Andres_Fonseca <affonseca9812@gmail.com>
2025-09-01 16:12:07 -0500

    Cambio scripts to railway

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegurar que la base sea la raíz
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
